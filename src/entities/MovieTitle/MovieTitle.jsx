9bc9454aecc55f4c1e51a3bac8cd23969da32a68
Anna <annamartis96@gmail.com>
2023-09-17 14:10:08 +0400

    MovieTitle Part Done

--- src/entities/MovieTitle/MovieTitle.jsx
+++ src/entities/MovieTitle/MovieTitle.jsx
@@ -1,4 +1,7 @@
 import "./MovieTitle.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export const MovieTitle = ({
   imgUrl,
@@ -7,10 +10,27 @@ export const MovieTitle = ({
   genres,
   onClickCB,
 }) => {
-  console.log("movieNamee", movieName);
+  const [isContextDropdownOpen, setContextDropdownOpen] = useState(false);
+
+  const handleToggleDropdown = () => {
+    setContextDropdownOpen((prevValue) => !prevValue);
+  };
+
   return (
     <div className="movie-title" onClick={onClickCB}>
       <img src={imgUrl} alt="movie" className="movie-image" />
+      <div className="context-menu">
+        <FontAwesomeIcon
+          icon={faEllipsisVertical}
+          className="context-icon"
+          onClick={handleToggleDropdown}
+        />
+        <div className={`items ${isContextDropdownOpen ? "active" : "hidden"}`}>
+          <p>Edit</p>
+          <p>Delete</p>
+        </div>
+      </div>
+
       <div className="movie-content">
         <div className="title">
           <p className="movie-name">{movieName}</p>
