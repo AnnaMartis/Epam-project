514f17af967f852e5ddb63f01106d8e2d8213d42
Anna <annamartis96@gmail.com>
2023-09-21 17:04:23 +0400

    cypress part done

--- cypress/e2e/counter.cy.js
+++ cypress/e2e/counter.cy.js
@@ -1,21 +1,26 @@
 describe('Counter Part', () => {
-  it('successfully loads', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000');
+    cy.wait(1000);
   });
-  
-  // it ('increments value when clicked Increment', ()=>{
-  //   const value = cy.get('.number');
-  //   cy.get('.increment').click();
-  //   cy.get('.number').should((number)=>{
-  //     expect(number).toBe(value + 1)
-  //   })
-  // });
 
-  // it ('decrements value when clicked Decrement', ()=>{
-  //   const value = cy.get('.number');
-  //   cy.get('.decrement').click();
-  //   cy.get('.number').should((number)=>{
-  //     expect(number).toBe(value + 1)
-  //   })
-  // });
-})
+  it('increments value when clicked Increment', () => {
+    cy.get('.counter')
+      .find('.number')
+      .invoke('text')
+      .then((value) => {
+        cy.get('.increment').click();
+        cy.get('.number').should('have.text', String(Number(value) + 1));
+      });
+  });
+
+  it('decrements value when clicked Decrement', () => {
+    cy.get('.counter')
+      .find('.number')
+      .invoke('text')
+      .then((value) => {
+        cy.get('.decrement').click();
+        cy.get('.number').should('have.text', String(Number(value) - 1));
+      });
+  });
+});
